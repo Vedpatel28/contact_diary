08c17b6df4672dfbf1deba4c116fa8ca66418153
Vedpatel28 <vedpatelflutter28@gmail.com>
2023-05-03 11:56:00 +0530

    VP | 3/5/2023 | # Work in RW Lab...

--- lib/views/modals/Global_varibles.dart
+++ lib/views/modals/Global_varibles.dart
@@ -1,21 +1,28 @@
 import 'dart:io';
 
 class allGlobalvar {
-
   // add page var
 
-  static File? image ;
+  static File? image;
+
+  static List<String>? Fnamelist;
   static String? Fname;
+  static List<String>? Lnamelist;
   static String? Lname;
+  static List<int>? Pnumberlist;
   static int? Pnumber;
-  static String? Email;
+
+  // static String? Email;
 
   // List var
 
-  static List<File> listofimage = [] ;
-  static List<String> listofFname = [] ;
-  static List<String> listofLname = [] ;
-  static List<int> listofPnumber = [] ;
-  static List<String> listofEmail = [] ;
+  static List<File> listofimage = [];
 
+  static List<String> listofFname = [];
+
+  static List<String> listofLname = [];
+
+  static List<int> listofPnumber = [];
+
+  static List<String> listofEmail = [];
 }
